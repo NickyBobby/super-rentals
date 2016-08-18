6a5c15efd09dbbef971fdea68fab21282faa8180
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-08-18 13:07:32 -0600

    Initial Commit

---
+++ mirage/config.js
@@ -0,0 +1,47 @@
+export default function() {
+  this.get('/rentals', function(db, request) {
+    let rentals = [{
+        type: 'rentals',
+        id: 1,
+        attributes: {
+          title: 'Grand Old Mansion',
+          owner: 'Veruca Salt',
+          city: 'San Francisco',
+          type: 'Estate',
+          bedrooms: 15,
+          image: 'https://s-media-cache-ak0.pinimg.com/originals/04/9d/da/049dda252de84dc6e7fb22f45434ed92.jpg'
+          }
+      }, {
+        type: 'rentals',
+        id: 2,
+        attributes: {
+          title: 'Urban Living',
+          owner: 'Mike Teavee',
+          city: 'Seattle',
+          type: 'Condo',
+          bedrooms: 1,
+          image: 'http://tcbmag.com/getattachment/Industries/Real-Estate/Twin-Cities-Condo-Comeback/0516-TwinCitiesCondo_S02.jpg'
+          }
+      }, {
+        type: 'rentals',
+        id: 3,
+        attributes: {
+          title: 'Downtown Charm',
+          owner: 'Violet Beauregarde',
+          city: 'Portland',
+          type: 'Apartment',
+          bedrooms: 3,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Tokyo_apartment.jpg'
+        }
+      }];
+
+    if(request.queryParams.city !== undefined) {
+      let filteredRentals = rentals.filter(function(i) {
+        return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1;
+      });
+      return { data: filteredRentals };
+    } else {
+      return { data: rentals };
+    }
+  });
+}
